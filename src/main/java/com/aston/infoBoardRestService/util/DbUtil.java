cb6be57b21a15a5750a868ee19cf4e9353dcd5f7
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-09-10 07:38:37 +0600

    add javadoc to DbUtil

--- src/main/java/com/aston/infoBoardRestService/util/DbUtil.java
+++ src/main/java/com/aston/infoBoardRestService/util/DbUtil.java
@@ -4,6 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Utility class for managing database connections.
+ * This class implements the Singleton design pattern to ensure that
+ * only one instance of the database utility exists throughout the application.
+ * It provides a method to obtain a connection to the PostgreSQL database.
+ */
 public class DbUtil {
     private static final String URL = "jdbc:postgresql://localhost:5431/info_board";
     private static final String USER = "postgres";
@@ -16,7 +22,12 @@ public class DbUtil {
     private DbUtil() {
     }
 
-    // Public method to provide access to the singleton instance
+    /**
+     * Returns the singleton instance of the DbUtil class.
+     * This method is synchronized to ensure thread safety.
+     *
+     * @return the singleton instance of DbUtil
+     */
     public static synchronized DbUtil getInstance() {
         if (instance == null) {
             instance = new DbUtil();
@@ -24,6 +35,12 @@ public class DbUtil {
         return instance;
     }
 
+    /**
+     * Obtains a connection to the PostgreSQL database.
+     *
+     * @return a Connection object to the database
+     * @throws SQLException if a database access error occurs
+     */
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
