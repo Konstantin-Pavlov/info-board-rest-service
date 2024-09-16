9b907fa34302f2a8d2f58fc20313670996f4d222
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-09-17 01:57:54 +0600

    upd

--- src/main/java/com/aston/infoBoardRestService/servlet/UserServlet.java
+++ src/main/java/com/aston/infoBoardRestService/servlet/UserServlet.java
@@ -4,6 +4,7 @@ import com.aston.infoBoardRestService.dto.UserDto;
 import com.aston.infoBoardRestService.service.UserService;
 import com.aston.infoBoardRestService.service.impl.UserServiceImpl;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -67,5 +68,21 @@ public class UserServlet extends HttpServlet {
         out.println("<br>");
         out.println("<br>  <a href=\"http://localhost:8080/\"\">homepage</a> <br>\n");
         out.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
     }
 }
