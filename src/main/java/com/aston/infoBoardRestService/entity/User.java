56558915b138d30d61ce59b6f7a96e218e74d10f
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-09-25 20:14:19 +0600

    add constructor, equals and hashcode

--- src/main/java/com/aston/infoBoardRestService/entity/User.java
+++ src/main/java/com/aston/infoBoardRestService/entity/User.java
@@ -1,6 +1,7 @@
 package com.aston.infoBoardRestService.entity;
 
 import java.util.List;
+import java.util.Objects;
 
 public class User {
     private Long id;
@@ -14,6 +15,13 @@ public class User {
     public User(String name, String email) {
         this.name = name;
         this.email = email;
+    }
+
+    public User(Long id, String name, String email, List<Message> messages) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.messages = messages;
     }
 
     public Long getId() {
@@ -47,5 +55,18 @@ public class User {
 
     public void setMessages(List<Message> messages) {
         this.messages = messages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(messages, user.messages);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, messages);
     }
 }
